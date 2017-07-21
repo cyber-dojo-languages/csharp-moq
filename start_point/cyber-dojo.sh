e54e50ca342461ee9b43bd0204dda476459fdac8
Jon Jagger <jon@jaggersoft.com>
2017-07-21 08:52:36 +0100

    upgraded Moq to 4.1.1 (and NUnit to 3.7.1)

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,13 +1,16 @@
-NUNIT_PATH=/moq/NUnit.2.6.2/lib
-MOQ_PATH=/moq/Moq.4.0.10827/lib/NET40
-export MONO_PATH=${NUNIT_PATH}:${MOQ_PATH}
+MOQ_PATH=/moq/Moq.4.7.99/lib/net45
+CASTLE_PATH=/moq/Castle.Core.4.1.1/lib/net45
+NUNIT_PATH=/moq/lib/net45
+
+export MONO_PATH=${MOQ_PATH}:${CASTLE_PATH}:${NUNIT_PATH}
 
 mcs -t:library \
-  -r:${NUNIT_PATH}/nunit.framework.dll \
   -r:${MOQ_PATH}/Moq.dll \
+  -r:${CASTLE_PATH}/Castle.Core.dll \
+  -r:${NUNIT_PATH}/nunit.framework.dll \
   -out:RunTests.dll *.cs
 
 if [ $? -eq 0 ]; then
-  NUNIT_RUNNERS_PATH=/moq/NUnit.Runners.2.6.1/tools
-  mono ${NUNIT_RUNNERS_PATH}/nunit-console.exe -nologo ./RunTests.dll
+  NUNIT_RUNNERS_PATH=/moq/tools
+  mono ${NUNIT_RUNNERS_PATH}/nunit3-console.exe --noheader ./RunTests.dll
 fi
