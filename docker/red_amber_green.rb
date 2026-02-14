40d9862b13b2beba49124ff5732f4139e502ea10
JonJagger <jon@kosli.com>
2026-02-14 08:34:41 +0000

    Update red-amber-green lambda

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /^\s*Test Count: \d+, Passed: \d+, Failed: [^0]\d*, Warnings: \d+, Inconclusive: \d+, Skipped: \d+/.match(output)
-  return :green if /^\s*Overall result: Passed/.match(output)
+  return :green if /^Passed!/.match(output)
+  return :red   if /^Failed!/.match(output)
   return :amber
 }
